0057ad674c5ea1420af19952f293627f8fa506c6
Jeremiah Windle <94581154+windlej@users.noreply.github.com>
2022-03-09 13:05:12 -0800

    Update 05_update_and_draw.py

--- arcade_platformer/05_update_and_draw.py
+++ arcade_platformer/05_update_and_draw.py
@@ -8,3 +8,48 @@ def on_draw(self) -> None:
     self.goals.draw()
     self.ladders.draw()
     self.player.draw()
+    
+def on_update(self, delta_time: float) -> None:
+    """Updates the position of all game objects
+
+    Arguments:
+        delta_time {float} -- How much time since the last call
+    """
+
+    # Update the player animation
+    self.player.update_animation(delta_time)
+
+    # Update player movement based on the physics engine
+    self.physics_engine.update()
+
+    # Restrict user movement so they can't walk off screen
+    if self.player.left < 0:
+        self.player.left = 0
+
+    # Check if we've picked up a coin
+    coins_hit = arcade.check_for_collision_with_list(
+        sprite=self.player, sprite_list=self.coins
+    )
+
+    for coin in coins_hit:
+        # Add the coin score to our score
+        self.score += int(coin.properties["point_value"])
+
+        # Play the coin sound
+        arcade.play_sound(self.coin_sound)
+
+        # Remove the coin
+        coin.remove_from_sprite_lists()
+
+    # Now check if we're at the ending goal
+    goals_hit = arcade.check_for_collision_with_list(
+        sprite=self.player, sprite_list=self.goals
+    )
+
+    if goals_hit:
+        # Play the victory sound
+        self.victory_sound.play()
+
+        # Set up the next level
+        self.level += 1
+        self.setup()
